d4f87d588fef55f9cfefc20b95e2426e8cd61817
Riska Adhita <Riska Adhita>
2016-10-13 15:49:03 +0700

    punya riska kedua

---
+++ app/src/main/java/com/example/qonita/farmarket/BuatPostJualToko.java
@@ -0,0 +1,13 @@
+package com.example.qonita.farmarket;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BuatPostJualToko extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buat_post_jual_toko);
+    }
+}
